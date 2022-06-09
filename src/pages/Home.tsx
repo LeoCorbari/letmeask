3ba51f93a8109b354e8c8e86785e491dbee0f8d8
unknown <leonardocorbari97@gmail.com>
2022-06-08 23:37:08 -0300

    add RoomCode

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -52,7 +52,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={handleCreateRoom}>
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
